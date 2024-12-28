d3a855121e4b1ba07386bae2bae05f1a32a40515
P-Odiix <mahingarodin@gmail.com>
2024-12-28 23:29:23 +0200

    "added the post room functionality"

---
+++ HotelWeb/src/app/auth/components/post-room/post-room.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-room',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-room.component.html',
+  styleUrl: './post-room.component.scss'
+})
+export class PostRoomComponent {
+
+}
