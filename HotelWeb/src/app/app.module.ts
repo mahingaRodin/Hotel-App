ff140be3b7a069224919d929674b30b211a0f8ee
P-Odiix <mahingarodin@gmail.com>
2025-01-01 18:49:40 +0200

    "updated the Reservation service implementation and controller"

--- HotelWeb/src/app/app.module.ts
+++ HotelWeb/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule'; // Ensure correct import path
+import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
-import { HttpClientModule } from '@angular/common/http';// Corrected import path
-import { AppRoutingModule } from './app.routes'; // Ensure correct path
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app.routes';
 import { NzIconModule, NzIconService } from 'ng-zorro-antd/icon';
-import { UserOutline, LoginOutline } from '@ant-design/icons-angular/icons'; // Import required icons
-import { Route, Router } from "@angular/router";
-
+import { UserOutline, LoginOutline } from '@ant-design/icons-angular/icons';
 
 @NgModule({
   imports: [
@@ -21,11 +19,11 @@ import { Route, Router } from "@angular/router";
     ReactiveFormsModule,
     BrowserAnimationsModule,
     NzIconModule,
-  ],// Bootstrap AppComponent
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
 })
 export class AppModule {
   constructor(private iconService: NzIconService) {
-    // Register required icons
     this.iconService.addIcon(UserOutline, LoginOutline);
   }
 }
