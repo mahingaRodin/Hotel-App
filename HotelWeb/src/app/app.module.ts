6248bc5197fbdd016614b762341df22d10a9a0c8
P-Odiix <mahingarodin@gmail.com>
2025-02-18 14:23:52 +0200

    "changed"

--- HotelWeb/src/app/app.module.ts
+++ HotelWeb/src/app/app.module.ts
@@ -1,7 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,6 +14,8 @@ import { AppRoutingModule } from './app.routes';
 import { NzIconModule, NzIconService } from 'ng-zorro-antd/icon';
 import { UserOutline, LoginOutline } from '@ant-design/icons-angular/icons';
 import { AuthService } from './auth/services/auth/auth.service';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 // Define the routes
 const routes: Routes = [
@@ -30,18 +31,25 @@ const routes: Routes = [
     DemoNgZorroAntdModule,
     ReactiveFormsModule,
     NzIconModule,
-    
   ],
-  declarations: [AppComponent],  
   providers: [
     { provide: NZ_I18N, useValue: en_US },
     provideHttpClient(withFetch()),
     AuthService,
+    importProvidersFrom(BrowserModule),
   ],
-  bootstrap: [AppComponent],
 })
 export class AppModule {
   constructor(private iconService: NzIconService) {
     this.iconService.addIcon(UserOutline, LoginOutline);
   }
 }
+
+// Bootstrap the standalone component separately
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    provideHttpClient(withFetch()),
+    AuthService,
+  ],
+}).catch((err) => console.error(err));
